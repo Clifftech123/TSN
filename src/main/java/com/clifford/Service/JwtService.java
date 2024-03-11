b5205f9f90fe9d025511779f136cbf2f420e8286
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2024-03-11 19:35:16 +0000

    Adding more logics and also creating the Application config class

--- src/main/java/com/clifford/Service/JwtService.java
+++ src/main/java/com/clifford/Service/JwtService.java
@@ -4,10 +4,15 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,6 +22,7 @@ public class JwtService {
 
 
     // extract user email from token
+    // This method takes a token and returns the email address of the user
     public String extractUserEmail(String token) {
         return extractClaim(token, Claims::getSubject);
     }
@@ -29,13 +35,53 @@ public class JwtService {
         return claimsResolver.apply(claims);
     }
 
-    // generate token for user
+
+    // generate token for user without extra claims
+    // This method takes a UserDetails object and returns a token
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
 
 
+    // generate token for user with extra claims
+    // This method takes a map of extra claims and a UserDetails object and returns a token
+    public String generateToken(
+            Map<String, Object> extraClaims,
+            UserDetails userDetails
+    ) {
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
 
+    // validate token
+    // This method takes a token and a UserDetails object and returns a boolean indicating whether the token is valid
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        final String userEmail = extractUserEmail(token);
+        return (userEmail.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    // check if the token is expired
+    // This method takes a token and returns a boolean indicating whether the token is expired
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+
+    }
+
+    // extract expiration date from token
+    // This method takes a token and returns the expiration date of the token
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
 
 
     // this method returns a Key object that is used to sign the token
+    // This method returns a Key object that is used to sign the token
     private Claims extractAllClaims(String token) {
         return Jwts
                 .parserBuilder()
@@ -47,6 +93,7 @@ public class JwtService {
 
 
     // Key object that is used to sign the token
+    // This method returns a Key object that is used to sign the token
     private Key getSignInKey() {
         byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
         return Keys.hmacShaKeyFor(keyBytes);
--- src/main/java/com/clifford/config/JwtAuthenticationFilter.java
+++ src/main/java/com/clifford/config/JwtAuthenticationFilter.java
@@ -7,6 +7,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -16,7 +21,9 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-     private final JwtService jwtService;
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+
     @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
@@ -28,24 +35,37 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         final String userEmail;
 
 
-        // If the token is null or does not start with "Bearer ", then the filter chain is continued
         if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
 
-            // The filter chain is continued and the request and response are passed to the next filter
             filterChain.doFilter(request, response);
             return;
 
         }
-
-        // The token is set to the substring of the authorization header starting at the 7th character
-        // This is the token without the "Bearer " prefix
         token = authorizationHeader.substring(7);
-
-        // The user email is set to the email address extracted from the token
-
         userEmail = jwtService.extractUserEmail(token);
 
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+
+            if (jwtService.validateToken(token, userDetails)) {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities()
+                );
+
+                authToken.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request)
+                );
+
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+
+            }
+
+        }
+        filterChain.doFilter(request, response);
 
     }
 
+
 }
+
+
